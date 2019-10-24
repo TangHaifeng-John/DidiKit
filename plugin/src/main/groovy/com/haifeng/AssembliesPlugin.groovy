5020d343e96de591d1d6e035e604ce2e48ea98eb
唐海峰 <haifengt@jumei.com>
2019-10-24 17:51:43 +0800

    first commit

---
+++ plugin/src/main/groovy/com/haifeng/AssembliesPlugin.groovy
@@ -0,0 +1,46 @@
+package com.haifeng
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AssembliesPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println("apply project : " + project.getName())
+
+
+        def haifeng = properties.get("haifeng")
+
+        System.out.println("测试参数" + haifeng)
+
+
+        project.task('haifeng') {
+            doLast {
+                println '唐海峰'
+            }
+        }
+//        project.dependencies {
+//
+//
+//            debugImplementation 'com.didichuxing.doraemonkit:doraemonkit:1.2.1'
+//            releaseImplementation 'com.didichuxing.doraemonkit:doraemonkit-no-op:1.2.0'
+//        }
+
+    }
+
+    /**
+     * 打印
+     * @param msg
+     */
+    void println(String msg) {
+        System.out.println("\n"
+                + "========================="
+                + "\n"
+                + "==" + msg
+                + "\n"
+                + "========================="
+                + "\n")
+    }
+
+}
